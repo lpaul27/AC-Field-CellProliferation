25ca4b33f6ccfcd562738c0b3cd2bb60d3676dc1
lpaul27 <lpaul27@LAB-SCI-202-02.whitworth.edu>
2025-07-31 14:59:27 -0700

    newest version

--- AC_cells_main.m
+++ AC_cells_main.m
@@ -13,34 +13,72 @@ global NumCells dt lbox vels_med eta nu neighborWeight k R_boundary Cell_radius 
     disphist velocity_noise sigmax sigmay directednessplot velocity_mag_noise displacement3by2 ...                                                                                               %#ok<GVMIS>
     densityplotDIR densityplotDISP densityDirTime
 
+%% Types of plots
+live = 0;                               % Enables Live Visualization
+dim1directionality = 0;                 % enables 1D Directionality plot
+dim2directionality = 0;                 % Enables 2D Directionality plot
+dim1displacement = 0;                   % Enables 1D Displacement plot
+displacement = 0;                       % Enables 2D Displacement plot
+polarhist = 0;                          % Enables polar histogram plot
+disphist =1;                           % enables displacement histogram plot
+directednessplot = 1;                   % enables directedness histogram plot
+displacement3by2 = 0;                   % enables displacement of all fields in a 3x2
+densityplotDIR = 0;                     % enables density plot for directionality
+densityplotDISP = 0;                   % enables density plot for displacement
+densityDirTime = 0;                     % enables directionality over time plot for density
+
+% Number of runs to be averaged across
+runs = 10;
+
+% begin start timer
 tStart = tic;
-runs = 20;
-fields = [0, 30, 50, 75,  100, 200];
-densityfields = [15, 30, 50, 75, 100, 200];
+
+%% constants found to make displacement plost match
 noises = [0.26, 0.24, 0.2, 0.28, 0.32, 0.56];
 forceScale = [1,1.11,1.4,0.85,0.77,0.45];
 noiseX = [0.28,0.28,0.23,0.33,0.37,0.61];
 v0 = [0.083, 0.11, 0.185, 0.07, 0.059, 0.026];
 nufric = [1, 1.15, 1.43, 0.93, 0.81,0.46];
 
+%% Declaration of strings for class for access in loops
+posxrun_str = ["posxr1", "posxr2", "posxr3", "posxr4", "posxr5", "posxr6"];
+posyrun_str = ["posyr1", "posyr2", "posyr3", "posyr4", "posyr5", "posyr6"];
 Directrun_str = ["direct1", "direct2", "direct3", "direct4", "direct5", "direct6"];
+
+%% Outer loop to control field strengths:
+% fields (normal)           densityfields(density):
+% 1: NO EF                  15 V/m
+% 2: 30 V/m                 30 V/m
+% 3: 50 V/m                 50 V/m
+% 4: 75 V/m                 75 V/m
+% 5: 100 V/m                100 V/m
+% 6: 200 V/m                200 V/m
+
+fields = [0, 30, 50, 75,  100, 200];
+densityfields = [15, 30, 50, 75, 100, 200];
+density = false;        % signals that a varying number of cells is intended
+% *default is 50 cells 
+
+%% Preallocation
 directedness = zeros(length(fields),1);
 dispAvg = zeros(length(fields),1);
 cell_posData = struct();
 cell_densitydata = struct();
-
 for z = 1:6
+    
+    % Preallocation for data storage
     displacement_run = zeros(runs, 1);
     displacementAvg = zeros(runs,  1);
     dispruntheta = zeros(runs,1);
 
+    %% Inner loop for number of runs at desired field strength
     for p = 1:runs
         % Begin Simulation timer
         %% Domain Parameters
         runTime = 120;                           % total runTime of simulation
         dt = 1;                                  % time step
         NumCells = 50;
-        if(0)
+        if(density)
             if(~mod(p,2))
                 NumCells =35;                           % number of cells in simulation
             end
@@ -48,14 +86,14 @@ for z = 1:6
                 NumCells = 210;
             end
         end
-        vels_med = 1;                        % initial velocity param center point
+        vels_med = 0.7;                        % initial velocity param center point
         lbox = 2050;                             % size of the box particles are confined to
-        R_boundary = lbox/6;                     % Sample domain size for cells to begin
+        R_boundary = 150; %lbox/6;                     % Sample domain size for cells to begin
         chill = 15;                              % chill time to suppress cell death
 
         %% Cell Parameters
         critRad = 12;                            % critical radius for mitosis
-        Ccyclet = 1300;                          % benchmark cell cycle time
+        Ccyclet = 1000;                      % benchmark cell cycle time
         death_rate = 1e-200;                     % Cell death rate
         death_pressure = 1000;                   % Pressure required for apoptosis
         critical_pressure = 0.05;                % Critical presssure for dormancy
@@ -66,12 +104,12 @@ for z = 1:6
         %% Cell-cell parameters
         k = 0.01;                               % constant in force repulsion calculation (~elasticity)
         daughter_noise = 0.1;                   % noise strength in mitosis separation
-        nu = nufric(1);                                 % friction factor
+        nu = 1;                                 % friction factor
         mu = 1;                                 % electrical mobility
         neighborWeight = 1;                  % group movement weighting
         c_rec = 0.9;                            % mean receptor concentration (normalized)
         c_lig = 0.9;                            % mean ligand concentration (normalized)
-        adh = 1e-4;                                % adhesive coefficient
+        adh = 0;                                % adhesive coefficient
 
         %% Cell-Field parameters
         % Discrete Parameters
@@ -79,9 +117,10 @@ for z = 1:6
         rand_division = 0;                      % Enables field-directed mitosis
         Discrete = 0;                           % Enables Discrete field change
 
-        ExMax = densityfields(z) / 2000;               % x field max
+        ExMax = fields(z) / 2500;               % x field max
         EyMax = 0;                              % y field max
         absE = sqrt(EyMax^2 + ExMax^2);         % magnitude of field
+        vels_med = vels_med*(1+fields(z)/200);                        % initial velocity param center point
 
 
         % Sinusoidal parameters
@@ -110,18 +149,7 @@ for z = 1:6
         G = ones(NumCells, 1);                  % Green scale for plotting
         B = ones(NumCells, 1);                  % Blue scale for plotting
 
-        live = 0;                               % Enables Live Visualization
-        dim1directionality = 0;                 % enables 1D Directionality plot
-        dim2directionality = 0;                 % Enables 2D Directionality plot
-        dim1displacement = 0;                   % Enables 1D Displacement plot
-        displacement = 0;                       % Enables 2D Displacement plot
-        polarhist = 0;                          % Enables polar histogram plot
-        disphist =1;                           % enables displacement histogram plot
-        directednessplot = 0;                   % enables directedness histogram plot
-        displacement3by2 = 0;                   % enables displacement of all fields in a 3x2
-        densityplotDIR = 0;                     % enables density plot for directionality
-        densityplotDISP = 0;                   % enables density plot for displacement
-        densityDirTime = 0;                     % enables directionality over time plot for density
+
 
         %% Initialization of Variables
         % Preallocates values for optimal computation
@@ -265,13 +293,13 @@ for z = 1:6
         x_centered = abs(x_time - x_time(1,:));
         y_centered = abs(y_time - y_time(1,:));
         displacement_mag = sqrt(x_centered.^2+y_centered.^2);
-%         displacementrunx = (xavg(runTime, 1) - xavg(1,1)) / runTime;
-%         displacementruny = (yavg(runTime, 1) - yavg(1,1)) / runTime;
-        displacement_run(p,1) = mean(displacement_mag(runTime, :)) / runTime;
+        %         displacementrunx = (xavg(runTime, 1) - xavg(1,1)) / runTime;
+        %         displacementruny = (yavg(runTime, 1) - yavg(1,1)) / runTime;
+        displacement_run(p,1) = mean(displacement_mag(runTime, :),2) / runTime;
 
         x_raw = (x_time - x_time(1,:));
         y_raw = (y_time - y_time(1,:));
-        dispruntheta = mean(cos(atan2(y_raw(runTime,:) , x_raw(runTime, :))));
+        dispruntheta = mean(cos(atan2(y_raw(runTime,:) , x_raw(runTime, :))),2);
         posxrun_str = ["posxr1", "posxr2", "posxr3", "posxr4", "posxr5", "posxr6"];
         posyrun_str = ["posyr1", "posyr2", "posyr3", "posyr4", "posyr5", "posyr6"];
         switch z
@@ -295,28 +323,28 @@ for z = 1:6
                 cell_posData(p).theta3 = theta_time;
                 y_tmp = cell_posData(p).posyr3;
                 x_tmp = cell_posData(p).posxr3;
-                cell_posData(p).direct3 = cos(atan2(y_tmp, x_tmp));                
+                cell_posData(p).direct3 = cos(atan2(y_tmp, x_tmp));
             case 4
                 cell_posData(p).posxr4 = x_time - x_time(1,:);
                 cell_posData(p).posyr4 = y_time - y_time(1,:);
                 cell_posData(p).theta4 = theta_time;
                 y_tmp = cell_posData(p).posyr4;
                 x_tmp = cell_posData(p).posxr4;
-                cell_posData(p).direct4 = cos(atan2(y_tmp, x_tmp)); 
+                cell_posData(p).direct4 = cos(atan2(y_tmp, x_tmp));
             case 5
                 cell_posData(p).posxr5 = x_time - x_time(1,:);
                 cell_posData(p).posyr5 = y_time - y_time(1,:);
                 cell_posData(p).theta5 = theta_time;
                 y_tmp = cell_posData(p).posyr5;
                 x_tmp = cell_posData(p).posxr5;
-                cell_posData(p).direct5 = cos(atan2(y_tmp, x_tmp)); 
+                cell_posData(p).direct5 = cos(atan2(y_tmp, x_tmp));
             case 6
                 cell_posData(p).posxr6 = x_time - x_time(1,:);
                 cell_posData(p).posyr6 = y_time - y_time(1,:);
                 cell_posData(p).theta6 = theta_time;
                 y_tmp = cell_posData(p).posyr6;
                 x_tmp = cell_posData(p).posxr6;
-                cell_posData(p).direct6 = cos(atan2(y_tmp, x_tmp)); 
+                cell_posData(p).direct6 = cos(atan2(y_tmp, x_tmp));
         end
 
 
@@ -324,9 +352,7 @@ for z = 1:6
     dispAvg(z,1) = mean(displacement_run);
     directedness(z,1) = (dispruntheta);
 end
-posxrun_str = ["posxr1", "posxr2", "posxr3", "posxr4", "posxr5", "posxr6"];
-posyrun_str = ["posyr1", "posyr2", "posyr3", "posyr4", "posyr5", "posyr6"];
-Directrun_str = ["direct1", "direct2", "direct3", "direct4", "direct5", "direct6"];
+
 
 for i = 1:length(fields)
     for j = 1:runs
